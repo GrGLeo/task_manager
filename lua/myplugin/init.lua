ddb3b179d18142a0dc54b84fbf7412e1bc6e0d6c
GrGLeo <graziani.gothe.leo@gmail.com>
2024-07-26 16:16:52 +0000

    adding command to open and close window

--- lua/myplugin/init.lua
+++ lua/myplugin/init.lua
@@ -4,8 +4,8 @@ local M = {}
 function M.open_floating_window()
   local opts = {
     relative = "editor",    -- Relative to the entire editor
-    width = 50,             -- Width of the window
-    height = 10,            -- Height of the window
+    width = 150,             -- Width of the window
+    height = 30,            -- Height of the window
     row = (vim.o.lines - 10) / 2, -- Center vertically
     col = (vim.o.columns - 50) / 2, -- Center horizontally
     style = "minimal",      -- Style of the window
@@ -20,6 +20,13 @@ function M.open_floating_window()
 
   -- Optionally set some window options
   vim.api.nvim_win_set_option(win, 'winhl', 'Normal:Normal')
+end
+
+function M.close_floating_window()
+  if current_win then
+    vim.api.nvim_win_close(current_win, true)
+    current_win = nil
+  end
 end
 
 return M
--- plugin/myplugin.vim
+++ plugin/myplugin.vim
@@ -1,1 +1,4 @@
 lua require('myplugin').open_floating_window()
+
+command! OpenFloatingWindow lua require('myplugin').open_floating_window()
+command! CloseFloatingWindow lua require('myplugin').close_floating_window()
